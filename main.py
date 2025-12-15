d4894f41c911e256a4057cf99acd13db66b43782
Brasske <markmofi@mail.ru>
2025-12-16 00:13:14 +0300

    fix

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 from fastapi import FastAPI
-from sqlalchemy.ext.asyncio import AsyncEngine
-from sqlalchemy.ext.asyncio import async_sessionmaker
-from sqlalchemy import select
-from sqlalchemy.ext.asyncio import create_async_engine
-
-from database import engine, Base, get_db
+from database import engine, Base
 from routers import auth, quests, user
-
-import asyncio
-
-
-
 
 app = FastAPI()
 
@@ -21,6 +11,5 @@ app.include_router(user.router)
 
 @app.on_event("startup")
 async def on_startup():
-    # создаём все таблицы через async engine
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
